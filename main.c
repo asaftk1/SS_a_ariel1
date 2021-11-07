8a3dd002ce764781c8b4b72292b81fc567c1732d
asaftk1 <93122554+asaftk1@users.noreply.github.com>
2021-11-07 16:05:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,22 +12,22 @@ int main()
     scanf("%d",&last);
         printf("The Armstrong numbers are: ");
       for(int i = first ; i<=last ; i++){
-        if(isArmstrong(i)==1)printf("%d ",i);
+        if(isArmstrong(i)==1)printf(" %d",i);
     }
     printf("\n");
     printf("The Palindrome numbers are: ");
     for(int i = first ; i<=last ; i++){
-        if(isPalindrome(i)==1)printf("%d ",i);
+        if(isPalindrome(i)==1)printf(" %d",i);
     }
     printf("\n");
     printf("The Prime numbers are: ");
       for(int i = first ; i<=last ; i++){
-        if(isPrime(i)==1)printf("%d ",i);
+        if(isPrime(i)==1)printf(" %d",i);
     }
     printf("\n");
     printf("The Strong numbers are: ");
       for(int i = first ; i<=last ; i++){
-        if(isStrong(i)==1)printf("%d ",i);
+        if(isStrong(i)==1)printf(" %d",i);
     }
     printf("\n");
    return 0; 
