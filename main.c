1726fb8780a382f66600ba6cdf532c2d7fafeaef
asaftk1 <93122554+asaftk1@users.noreply.github.com>
2021-11-07 17:11:21 +0200

    Update main.c

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main()
         if(isArmstrong(i)==1)printf(" %d",i);
     }
     printf("\n");
-    printf("The Palindrome numbers are:");
+    printf("The Palindromes numbers are:");
     for(int i = first ; i<=last ; i++){
         if(isPalindrome(i)==1)printf(" %d",i);
     }
